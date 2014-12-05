f4672e379c6b4450eb39d3b923bd65882dbb6f09
Klee <klee.ut@gmail.com>
2014-12-05 21:01:29 +1100

    Added error handling.

--- server.js
+++ server.js
@@ -4,36 +4,37 @@ var url = require('url');
 var provider = require('./static/StaticResourceProvider');
 
 server.on('request', function(request, response){
-    var requestMap = {
-        'favicon.ico':favIcon,
-        'static':staticTextResourceResponder,
-        "":indexResponseHandler
-    };
+    try {
+        var requestUrl = url.parse(request.url);
+        var pathName = requestUrl.pathname;
 
-    var requestUrl = url.parse(request.url);
-    var pathName = requestUrl.pathname;
+        console.log("Received request for path: " + pathName);
 
-    console.log("Received request for path: " + pathName);
+        var pathComponents = getPathComponents(pathName);
+        var pathToRespondTo = pathComponents[0];
+        var responder;
 
-    var pathComponents = getPathComponents(pathName);
-    var pathToRespondTo = pathComponents[0];
-    var responder;
+        console.log(pathToRespondTo + pathComponents.toString());
+        if (pathToRespondTo == undefined) {
+            responder = requestMap[""]
+        }
+        else {
+            responder = requestMap[pathToRespondTo];
+        }
 
-    console.log(pathToRespondTo + pathComponents.toString());
-    if(pathToRespondTo == undefined){
-        responder = requestMap[""]
-    }
-    else{
-        responder = requestMap[pathToRespondTo];
-    }
-
-    if(responder == null || responder == undefined) {
-        response.writeHead(404);
-        var message = "Could not find responder for " + pathToRespondTo + ' it was ' + responder;
-        response.end(message);
-        console.log(message);
-    }else{
-        responder(request, response);
+        if (responder == null || responder == undefined) {
+            response.writeHead(404);
+            var message = "Could not find responder for " + pathToRespondTo + ' it was ' + responder;
+            response.end(message);
+            console.log(message);
+        } else {
+            responder(request, response);
+        }
+    }catch(err){
+        console.log("error processing request " + request);
+        console.log(err);
+        response.writeHead(500);
+        response.end("Exception occured return to <a href='/'>home</a>");
     }
 });
 
